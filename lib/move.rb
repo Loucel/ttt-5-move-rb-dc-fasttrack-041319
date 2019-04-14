050d6857934fe4b0f1fbc44c893113bc11008704
Loucel Torreon <loucelp@yahoo.com>
2019-04-14 15:51:58 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,7 +1,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 
+
 def display_board(board)
+#>>>>>>> 29c5d2b5c906ae650e158ecac444283577653b8c
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
